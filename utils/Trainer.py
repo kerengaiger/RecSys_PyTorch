1f848848b8d8279af595f3b9805bfddf20509b14
kerengaiger <keren.gaiger@gmail.com>
2021-07-21 09:47:29 +0300

    Trainer: optimize by loss

--- utils/Trainer.py
+++ utils/Trainer.py
@@ -47,7 +47,7 @@ class Trainer:
             epoch, self.num_epochs, epoch_elapsed, train_elapsed, loss, score_str))
 
             # update if ...
-            standard = 'NDCG@100'
+            standard = 'loss'
             if self.best_score is None or score[standard] >= self.best_score[standard]:
                 self.best_epoch = epoch
                 self.best_score = score
