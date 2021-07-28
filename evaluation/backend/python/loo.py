24c3b69bffacc0d4f16137935b9b88565b39aca5
kerengaiger <keren.gaiger@gmail.com>
2021-07-28 19:20:14 +0300

    llo: add hr and rr metrics, write to out files

--- evaluation/backend/python/loo.py
+++ evaluation/backend/python/loo.py
@@ -13,19 +13,27 @@ def compute_loo_metrics_py(pred, target, ks):
         score_cumulator[metric] = {k: Statistics('%s@%d' % (metric, k)) for k in ks}
     
     max_k = max(ks)
-    for idx, u in enumerate(target):
-        pred_u = pred[idx]
-        target_u = target[u][0]
-        
-        hit_at_k = np.where(pred_u == target_u)[0][0] + 1 if target_u in pred_u else max_k + 1
+    with open('hr_out', 'w') as hr_file, open('rr_out','w') as rr_file:
+        for idx, u in enumerate(target):
+            pred_u = pred[idx]
+            target_u = target[u][0]
 
-        for k in ks:
-            hr_k = 1 if hit_at_k <= k else 0
-            ndcg_k = 1 / math.log(hit_at_k + 1, 2) if hit_at_k <= k else 0
-            mrr_k = hit_at_k if hit_at_k <= k else 0
+            hit_at_k = np.where(pred_u == target_u)[0][0] + 1 if target_u in pred_u else max_k + 1
 
-            score_cumulator['HR'][k].update(hr_k)
-            score_cumulator['NDCG'][k].update(ndcg_k)
-            score_cumulator['MRR'][k].update(mrr_k)
+            hr_ks = []
+            rr_ks = []
+            for k in ks:
+                hr_k = 1 if hit_at_k <= k else 0
+                ndcg_k = 1 / math.log(hit_at_k + 1, 2) if hit_at_k <= k else 0
+                rr_k = hit_at_k if hit_at_k <= k else 0
+                hr_ks.append(str(hr_k))
+                rr_ks.append(str(rr_k))
+
+                score_cumulator['HR'][k].update(hr_k)
+                score_cumulator['NDCG'][k].update(ndcg_k)
+                score_cumulator['MRR'][k].update(rr_k)
+
+            hr_file.write(','.join(hr_ks))
+            rr_file.write(','.join(rr_ks))
 
     return score_cumulator
