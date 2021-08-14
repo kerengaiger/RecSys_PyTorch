ba6be0ac06962fa882839fd9b9de39f5aae3a377
kerengaiger <keren.gaiger@gmail.com>
2021-08-14 18:21:53 +0300

    config: add type in arg

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ class EvaluatorConfig:
 class EarlyStopConfig:
     early_stop:int=40
     early_stop_measure:str='HR@20'
-    is_final_train=True
+    is_final_train:bool=True
 
 @dataclass
 class ExperimentConfig:
