767782f997debd2a918dbb8cd88e2aac2c700d96
kerengaiger <keren.gaiger@gmail.com>
2021-08-24 17:52:25 +0300

    llo:debug prints

--- evaluation/backend/python/loo.py
+++ evaluation/backend/python/loo.py
@@ -27,6 +27,7 @@ def compute_loo_metrics_py(pred, target, ks, usermap_file, itemmap_file):
         target_u = target[u][0]
 
         print(target_u)
+        print(pred_u)
         hit_at_k = np.where(pred_u == target_u)[0][0] + 1
         hits_at_k.append(hit_at_k)
         users.append(u)
