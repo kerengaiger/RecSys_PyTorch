3c2c21922976f91edb7fa27f873b88e84e2c0205
kerengaiger <keren.gaiger@gmail.com>
2021-09-13 10:27:20 +0300

    tune: Objective class and save best cnfg

--- tune.py
+++ tune.py
@@ -11,8 +11,8 @@ from utils.general import make_log_dir, set_random_seed
 from config import load_config
 
 import optuna
+import pickle
 import argparse
-from omegaconf import OmegaConf
 
 
 def train_with_conf(hparams_cnfg, trial=None):
@@ -83,32 +83,48 @@ def parse_args():
     return parser.parse_args()
 
 
-def objective(trial):
-    cnfg = {}
-    args = parse_args()
-    if args.model == 'LightGCN':
-        cnfg['split'] = False
-        cnfg['num_folds'] = 100
-        cnfg['graph_dir'] = 'graph'
-        cnfg['reg'] = 0.0001
-        cnfg['use_validation'] = True
-        cnfg['node_dropout'] = trial.suggest_float("node_dropout", 0.1, 0.6)
-        cnfg['emb_dim'] = trial.suggest_int("emb_dim", 10, 120, step=4)
-        cnfg['num_layers'] = trial.suggest_int("num_layers", 2, 5, step=1)
-    else:
-        cnfg['pointwise'] = False
-        cnfg['use_validation'] = True
-        cnfg['hidden_dim'] = trial.suggest_int("hidden_dim", 10, 100, step=4)
-        cnfg['lr'] = trial.suggest_float("lr", 5e-3, 1e-1)
-        cnfg['loss_func'] = trial.suggest_categorical("loss_func", ['ce', 'mse'])
-    hr_20 = train_with_conf(cnfg, trial)
-    return hr_20
+class Objective:
+    def __init__(self):
+        self.model = None
+        config = load_config()
+        self.exp_config = config.experiment
+        self.dataset_config = config.dataset
+
+    def __call__(self, trial):
+        cnfg = {}
+        if self.model == 'LightGCN':
+            cnfg['split'] = False
+            cnfg['num_folds'] = 100
+            cnfg['graph_dir'] = 'graph'
+            cnfg['reg'] = 0.0001
+            cnfg['use_validation'] = True
+            cnfg['node_dropout'] = trial.suggest_float("node_dropout", 0.1, 0.6)
+            cnfg['emb_dim'] = trial.suggest_int("emb_dim", 10, 120, step=4)
+            cnfg['num_layers'] = trial.suggest_int("num_layers", 2, 5, step=1)
+        else:
+            cnfg['pointwise'] = False
+            cnfg['use_validation'] = True
+            cnfg['hidden_dim'] = trial.suggest_int("hidden_dim", 10, 100, step=4)
+            cnfg['lr'] = trial.suggest_float("lr", 5e-3, 1e-1)
+            cnfg['loss_func'] = trial.suggest_categorical("loss_func", ['ce', 'mse'])
+        hr_20 = train_with_conf(cnfg, trial)
+        return hr_20
+
+    def callback(self, study, trial):
+        if study.best_trial == trial:
+            best_cnfg = trial.params
+            pickle.dump(best_cnfg, open(os.path.join(self.exp_config.save_dir,
+                                                     f'{self.dataset_config.dataname}_{self.model}_cnfg_pkl', 'wb')))
 
 
 def main():
     args = parse_args()
-    study = optuna.create_study(direction="maximize")
-    study.optimize(objective, n_trials=args.trials)
+    objective = Objective()
+
+    study = optuna.create_study(
+        pruner=optuna.pruners.MedianPruner(n_warmup_steps=10), direction="maximize"
+    )
+    study.optimize(objective, n_trials=args.trials, callbacks=[objective.callback])
 
     pruned_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.PRUNED]
     complete_trials = [t for t in study.trials if t.state == optuna.structs.TrialState.COMPLETE]
